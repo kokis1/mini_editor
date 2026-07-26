b3f3d5174e3199f56685e89fbbea23f1eb19c68f
kokis1 <reuben.stannah@icloud.com>
2026-07-26 10:29:16 +0100

    got prompt message for saving to disc

--- kilo.c
+++ kilo.c
@@ -69,6 +69,7 @@ struct EditorConfig E;
 
 /* PROTOTYPES */
 void editor_status_msg(const char *fmt, ...);
+void editor_refresh_screen();
 
 /* TERMINAL */
 
@@ -413,7 +414,36 @@ void ab_free(struct abuf *ab) {
 }
 
 /* INPUT */
+char *editor_prompt(char *prompt) {
+size_t bufsize = 128;
+char *buf = malloc(bufsize);
 
+size_t buflen = 0;
+buf[0] = '\0';
+
+while (1) {
+editor_status_msg(prompt, buf);
+editor_refresh_screen();
+
+int c = editor_read_key();
+
+if (c == '\r') {
+	if (buflen != 0) {
+		editor_status_msg("");
+		return buf;
+	}
+} else if (!iscntrl(c) && c < 128) {
+	if (buflen == bufsize - 1) {
+		bufsize *= 2;
+		buf = realloc(buf, bufsize);
+	}		 
+	buf[buflen++] = c;
+	buf[buflen] = '\0';
+}
+
+}
+
+}
 void editor_move_cursor(int key) {
    erow *row = (E.cy >= E.num_rows) ? NULL : &E.row[E.cy];
 
