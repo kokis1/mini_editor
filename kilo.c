dc44a889d85b71a79fad65517287ff87cdeaad5f
kokis1 <reuben.stannah@icloud.com>
2026-07-26 10:17:09 +0100

    fixed a CATASTROPHIC bug that caused the entire thing to wiped upon save

--- kilo.c
+++ kilo.c
@@ -325,6 +325,7 @@ char *editor_row_tostr(int *buflen) {
    int j;
    for (j = 0; j < E.num_rows; j++)
       tot_len += E.row[j].size + 1;
+   
    *buflen = tot_len;
 
    char *buf = malloc(tot_len);
@@ -373,8 +374,8 @@ void editor_save() {
 
    int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
    if (fd != -1) {
-      if (ftruncate(fd, len != -1)) {
-         if (write(fd, buf, len) != -1) {
+      if (ftruncate(fd, len) != -1) {
+         if (write(fd, buf, len) == len) {
             close(fd);
             free(buf);
             E.dirty = 0;
